56e960fc9d7296758d4366ce846f7b9170f30089
Kishore Atreya <kishoreatreya@Kishores-MacBook-Pro.local>
2018-05-23 08:51:53 -0700

    Adding reverse ll files

---
+++ linkedlists/reverse/reverse.c
@@ -0,0 +1,85 @@
+#include "reverse.h"
+
+node* allocNode()
+{
+	node* link = (node*) malloc(sizeof(node));
+	return link;
+}
+
+void destroyList(node* list)
+{
+	node* cur = list;
+	
+	while(cur != NULL)
+	{
+		node *tmp = cur;
+		cur = cur->next;
+		
+		free(tmp);
+	}
+}
+
+void printList(node * list)
+{
+	node* cur = list;
+	while(cur != NULL)
+	{
+		printf("data: %d\n", cur->data);
+		cur = cur->next;
+	}
+}
+
+node* buildListMult(uint32_t mux, uint32_t numNodes)
+{
+	// Builds a sorted list of numNodes with a skip of mux
+	node* root = NULL;
+	node* cur = NULL;
+		
+	root = allocNode();
+	root->data = mux;
+	cur = root;
+	
+	for(uint32_t i = 1; i <= numNodes; i++)
+	{
+		cur->next = allocNode();
+		cur = cur->next;
+		cur->data = (mux * (i+1));
+	}
+	cur->next = NULL;
+	
+	return root;
+}
+
+node* reverse(node* list)
+{
+	// This function will reverse a singly linked list
+	node* head = list;
+	node* cur = head->next;
+	node* tmp = NULL;
+	
+	while(head->next != NULL)
+	{
+		tmp = cur->next;
+		cur->next = head;
+		head->next = tmp;
+		cur = tmp;
+	}
+	
+	return cur;
+}
+
+int main (int argc, char* argv[])
+{
+	node* l0 = NULL;
+	node* rl0 = NULL;
+	
+	l0 = buildListMult(4, 4);
+	printf("forward: \n\n");
+	printList(l0);
+
+	rl0 = reverse(l0);
+	
+	printf("reverse: \n\n");
+	printList(rl0);
+
+}
---
+++ linkedlists/reverse/reverse.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+
+/**
+ * \brief linked list node
+ */
+typedef struct _node 
+{
+	uint32_t data;
+	struct _node *next;
+} node;
+
